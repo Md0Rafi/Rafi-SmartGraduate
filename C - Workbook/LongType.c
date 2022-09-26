2c2ca8796245bc50582b363a97e7992fbd19d537
Md0Rafi <112495437+Md0Rafi@users.noreply.github.com>
2022-09-26 20:05:41 +0530

    Add files via upload

---
+++ C - Workbook/LongType.c
@@ -0,0 +1,23 @@
+//LONG KEYWORD
+
+#include <stdio.h>
+
+int main()
+
+{
+
+ int a;
+ long b; // Equivalent to long int b;
+ long long c; // Equivalent to long long int c;
+ double e;
+ long double f;
+
+ printf("Size of int = %zu bytes \n", sizeof(a));
+ printf("Size of long int = %zu bytes\n", sizeof(b));
+ printf("Size of long long int = %zu bytes\n", sizeof(c));
+ printf("Size of double = %zu bytes\n", sizeof(e));
+ printf("Size of long double = %zu bytes\n", sizeof(f));
+
+ getch();
+
+}
