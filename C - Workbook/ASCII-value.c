747ed33dbc087ff8829e351f2cc8d3103d21044f
Md0Rafi <112495437+Md0Rafi@users.noreply.github.com>
2022-09-26 18:52:21 +0530

    Add files via upload

---
+++ C - Workbook/ASCII-value.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+ char chr;
+
+ printf("Enter a character: ");
+ scanf("%c", &chr);
+
+ // When %c is used, a character is displayed
+ printf("You entered %c.\n",chr);
+ // When %d is used, ASCII value is displayed
+ printf("ASCII value is %d.", chr);
+
+ getch();
+
+}
