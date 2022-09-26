25275cce54c60e0f31f50794adbe1a03559c1239
Md0Rafi <112495437+Md0Rafi@users.noreply.github.com>
2022-09-26 19:15:54 +0530

    Add files via upload

---
+++ C - Workbook/Inc-Dec.c
@@ -0,0 +1,18 @@
+// INCREMENT AND DECREMENT OPERATORS
+#include <stdio.h>
+
+void main()
+
+{
+
+ int a = 10;
+ float b = 10.5;
+
+ printf("a++ = %d \n", a++); //a++ First prints and then increments
+ printf("++a = %d \n", ++a); //++a First increments and then prints
+ printf("b-- = %f \n", b--); //b-- First prints and then decrements
+ printf("--b = %f \n", --b); //--b First decrements and then prints
+
+ getch();
+
+}
