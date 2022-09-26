1d8e3108244a3ea30c2d6907667df0b30d2e92b5
Md0Rafi <112495437+Md0Rafi@users.noreply.github.com>
2022-09-26 18:57:48 +0530

    Add files via upload

---
+++ C - Workbook/Addition.c
@@ -0,0 +1,17 @@
+//Addition of two integers
+#include<stdio.h>
+
+void main()
+
+{
+
+ int a, b, c;
+
+ printf("Enter two numbers to add\n");
+ scanf("%d%d",&a,&b);
+ c = a + b;
+ printf("Sum of two numbers = %d\n",c);
+
+ getch();
+
+}
