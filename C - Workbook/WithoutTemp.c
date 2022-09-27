49edf6f27b8a22895bb05e1901d8f12cc51f0025
Md0Rafi <112495437+Md0Rafi@users.noreply.github.com>
2022-09-27 20:07:48 +0530

    Add files via upload

---
+++ C - Workbook/WithoutTemp.c
@@ -0,0 +1,31 @@
+//Swapping two elements without using temp
+
+#include<stdio.h>
+
+void main()
+
+{
+
+    double a,b;
+
+    printf("Enter a: ");
+    scanf("%lf", &a);
+    printf("Enter b: ");
+    scanf("%lf", &b);
+
+    // a = (initial_a - initial_b)
+    a = a - b;
+
+    // b = (initial_a - initial_b) + initial_b = initial_a
+    b = a + b;
+
+    // a = initial_a - (initial_a - initial_b) = initial_b
+    a = b - a;
+
+    // %.2lf displays numbers up to 2 decimal places
+    printf("After swapping, a = %.2lf\n", a);
+    printf("After swapping, b = %.2lf", b);
+
+    getch();
+
+}
