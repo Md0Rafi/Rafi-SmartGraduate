802badc0ffb8d8f92ef7be27d4421514130337a1
Md0Rafi <112495437+Md0Rafi@users.noreply.github.com>
2022-09-27 20:09:38 +0530

    Add files via upload

---
+++ C - Workbook/If.c
@@ -0,0 +1,20 @@
+//Program using if to display only negative numbers
+
+#include<stdio.h>
+
+void main()
+
+{
+
+    int a,b;
+
+    printf("Enter an integer :");
+    scanf("%d",&a);
+    if(a<0)
+    {
+        printf("It is a negative number.");
+    }
+
+    getch();
+
+}
