35fffe6b12b5415173c09a00f0299add691f78b9
Md0Rafi <112495437+Md0Rafi@users.noreply.github.com>
2022-09-28 06:20:38 +0530

    Add files via upload

---
+++ C - Workbook/Ternary-Op.c
@@ -0,0 +1,18 @@
+//PROGRAM USING TERNARY OPERATOR
+
+#include<stdio.h>
+
+void main()
+
+{
+
+    int num;
+
+    printf("Enter a number :");
+    scanf("%d",&num);
+
+    (num % 2 == 0)? printf("%d is even",num) : printf("%d is odd",num);
+
+    getch();
+
+}
