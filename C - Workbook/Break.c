1dcd193759258b29cadf937aad65bea4f1c1743f
Md0Rafi <112495437+Md0Rafi@users.noreply.github.com>
2022-09-28 06:17:07 +0530

    Add files via upload

---
+++ C - Workbook/Break.c
@@ -0,0 +1,28 @@
+//PROGRAM TO BREAK A CONTROL FLOW
+
+#include <stdio.h>
+
+void main()
+
+{
+
+    int i;
+    double number, sum = 0.0;
+
+    for (i = 1; i <= 10; ++i)
+    {
+        printf("Enter n%d: ", i);
+        scanf("%lf", &number);
+
+        // If the user enters a negative number, break the loop
+        if (number < 0.0)
+        {
+            break;
+        }
+        sum += number; // sum = sum + number;
+    }
+    printf("Sum = %.2lf", sum);
+
+    getch();
+
+}
